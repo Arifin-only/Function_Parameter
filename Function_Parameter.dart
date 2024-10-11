9a7824e6d442e289e2e91df11b387f0798ba281f
Arifin-only <arifin180104@gmail.com>
2024-10-11 14:23:19 +0800

    Create Function_Parameter.dart

---
+++ Function_Parameter.dart
@@ -0,0 +1,12 @@
+void sayHello(String firstName, String lastName){
+
+    print('Hello $firstName $lastName');
+
+}
+
+void main(){
+
+    sayHello('Muhammad', 'Arifin');
+    
+    sayHello('Upin', 'Ipin');
+}
